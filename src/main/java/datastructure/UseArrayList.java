479707d2dd96893427614bd4b627340c3eb31bad
imran33bd <imran_nsu2003@hotmail.com>
2020-08-30 16:54:44 -0400

    update

--- src/main/java/datastructure/UseArrayList.java
+++ src/main/java/datastructure/UseArrayList.java
@@ -1,8 +1,5 @@
-package datastructure;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+package datastructure;
 
 public class UseArrayList {
 
@@ -16,46 +13,10 @@ public class UseArrayList {
          *
          */
 
-        int [] myArray=new int[5];
-        myArray[0]=12;
 
-        List<Integer> studentsIdList = new ArrayList<Integer>();
-        String name= new String("john");
-        System.out.println(name);
-        // ArrayList studentsIdList=new ArrayList<Integer>();
-        studentsIdList.add(11);
-        studentsIdList.add(12);
-        studentsIdList.add(13);
-        studentsIdList.add(14);
-        studentsIdList.add(null);
-        studentsIdList.add(11);
-        System.out.println("Element in index 0 : "+ studentsIdList.get(0));
-        System.out.println("Length of this List: "+ studentsIdList.size());
-        for(int i=0; i<=studentsIdList.size()-1;i++){
-            System.out.println("Element index "+ i + ":"+ studentsIdList.get(i));
-        }
-        for(Integer x: studentsIdList){
-            System.out.println(x);
-        }
-        // Collections.sort(studentsIdList);
-        ArrayList<String> cars = new ArrayList<String>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Mazda");
-        System.out.println(cars);
-        //System.out.println(cars.indexOf(cars));
-        System.out.println(cars.get(0));
-        cars.set(0, "Opel");
-        cars.remove(0);
-        System.out.println(cars.get(0));
-        System.out.println(cars.size());
-        for (String i : cars) {
-            System.out.println(i);
-        }
-        studentsIdList.forEach(i -> System.out.println("id : "+i));
-//foreach method
     }
 
 }
+
+
 
