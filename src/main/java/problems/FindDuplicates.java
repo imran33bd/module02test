20584045083809ac9119158689ae1cefe991f848
imran33bd <imran_nsu2003@hotmail.com>
2020-08-31 00:44:23 -0400

    update

--- src/main/java/problems/FindDuplicates.java
+++ src/main/java/problems/FindDuplicates.java
@@ -1,9 +1,12 @@
 package problems;
 
+import com.sun.tools.javac.util.ArrayUtils;
+
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class FindDuplicates {
 
@@ -12,7 +15,23 @@ public class FindDuplicates {
      * */
 
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+        List li1;
+        li1 = new ArrayList<>(Arrays.asList(
+                1,
+                3,
+                3,
+                5,
+                5,
+                2,
+                4,
+                6,
+                8));
+        System.out.println("Before removing duplicate elements: " + li1);
+        // create new list from elements of original list
+        List li2 = (List) li1.stream().distinct().collect(Collectors.toList());
+        // printing ArrayList with duplicates removed
+        System.out.println("After removing duplicate elements: " + li2);
     }
 }
